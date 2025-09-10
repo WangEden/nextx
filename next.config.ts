bd021bd0b1ee45c18a94428100c575cd99f3da86
WangEden <wangeden_@outlook.com>
2025-09-10 16:33:14 +0800

    .

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   // reactStrictMode: false,
+  // 显式允许哪些域名在开发环境访问 _next/* 资源
+  allowedDevOrigins: ["https://wangeden.top"], // 允许访问的域名列表
+  
 };
 
 export default nextConfig;
