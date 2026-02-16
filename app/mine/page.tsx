47124aa08aa98d2d3590f26fe9d1e2b81850b847
WangEden <68456937+WangEden@users.noreply.github.com>
2026-02-16 14:19:29 +0800

    update motto

--- app/mine/page.tsx
+++ app/mine/page.tsx
@@ -3,12 +3,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      下一步计划：
-      1.机械臂文章补全；
-      2.整理矩阵、数值分析笔记文章；
-      3.OpenGL相关笔记文章；
-      4.MC开服教程文章；
-      5.应用程序功能实现；
+      生活就像海洋，只有意志坚定的人才能到达彼岸。
     </div>
   )
 }
